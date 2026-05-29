c94becf09000016f11db43a825d0ffe45832cc60
jaadujaadujaadu0-alt <jaadujaadujaadu0@gmail.com>
2026-05-29 23:24:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
 import requests
+import os
 import re
 import time
 from datetime import datetime
@@ -301,4 +302,5 @@ def logout():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
